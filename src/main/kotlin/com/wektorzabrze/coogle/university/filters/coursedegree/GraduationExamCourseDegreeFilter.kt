53aa2261a5af3ae8b71fdc828800a9f97f8241fa
Paweł Oczadly <oczadlypawe@gmail.com>
2023-10-01 01:22:11 +0200

    User finals score (#3)
    * added endpoint to save user's exam scores
    
    * updated example universities

--- src/main/kotlin/com/wektorzabrze/coogle/university/filters/coursedegree/GraduationExamCourseDegreeFilter.kt
+++ src/main/kotlin/com/wektorzabrze/coogle/university/filters/coursedegree/GraduationExamCourseDegreeFilter.kt
@@ -1,31 +1,41 @@
 package com.wektorzabrze.coogle.university.filters.coursedegree
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.wektorzabrze.coogle.infrastructure.asList
 import com.wektorzabrze.coogle.university.CourseDegree
-import com.wektorzabrze.coogle.university.ExamThreshold
-import com.wektorzabrze.coogle.university.GraduateExamThreshold
+import com.wektorzabrze.coogle.user.service.UserService
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
 @Component
-class GraduationExamCourseDegreeFilter(val objectMapper: ObjectMapper) : CourseDegreeSearchFilter {
+class GraduationExamCourseDegreeFilter(val objectMapper: ObjectMapper, private val userService: UserService) :
+    CourseDegreeSearchFilter {
 
-    override fun filter(courseDegrees: Collection<CourseDegree>, filterValue: String): Collection<CourseDegree> =
-        courseDegrees
+    override fun filter(courseDegrees: Collection<CourseDegree>, filterValue: String): Collection<CourseDegree> {
+        val isSearchingByExamScores = objectMapper.readValue(filterValue, object : TypeReference<Boolean>() {})
+        if (!isSearchingByExamScores) {
+            return courseDegrees
+        }
+
+        val userScoreBySubject = userService.getUserFinals()
+
+        return courseDegrees
             .filter { courseDegree ->
-                val examResults = mapFilterValue(filterValue)
-                val intResult = examResults
-                    .map { graduateExam -> (courseDegree.examThreshold.subjectToWeightMap.getOrDefault(graduateExam.subject.lowercase(), BigDecimal.ZERO).times(graduateExam.result))}
+
+                val intResult = userScoreBySubject
+                    .map { graduateExam ->
+                        (courseDegree.examThreshold.subjectToWeightMap.getOrDefault(
+                            graduateExam.key,
+                            BigDecimal.ZERO
+                        ).times(graduateExam.value.percentage.toBigDecimal()))
+                    }
                     .reduce { acc, next -> acc + next }.toInt()
                 intResult >= courseDegree.examThreshold.minThreshold.toInt()
             }
+    }
+
     override val discriminator: String
         get() = "graduationExamResults"
-
-    private fun mapFilterValue(filterValue: String): List<GraduateExamThreshold> =
-        objectMapper.asList(filterValue, GraduateExamThreshold::class.java)
 }
 
 
