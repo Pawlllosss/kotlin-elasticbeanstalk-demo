1b54f8feba3f35db62171059d7001aeca46042cc
jkowalski <jkowalski@apptio.com>
2023-09-30 17:56:17 +0200

    revert test

---
+++ src/test/kotlin/pl/oczadly/ebs/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.oczadly.ebs.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
