85c17aff4dfc58d1dc325ea1d86291cd338dd41f
Pawel Oczadly <oczadlypawe@gmail.com>
2023-09-30 23:08:11 +0200

    fixed json mapping for search controller

--- src/main/kotlin/com/wektorzabrze/coogle/search/controller/SearchController.kt
+++ src/main/kotlin/com/wektorzabrze/coogle/search/controller/SearchController.kt
@@ -1,5 +1,7 @@
 package com.wektorzabrze.coogle.search.controller
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.wektorzabrze.coogle.search.service.SearchService
 import com.wektorzabrze.coogle.university.University
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/search")
-class SearchController(private val service: SearchService) {
+class SearchController(private val service: SearchService, private val objectMapper: ObjectMapper) {
 
     @PostMapping("/universities")
-    fun searchUniversities(@RequestBody searchParameters: Map<String, String>): Collection<University> {
-        
-        return service.search(searchParameters)
+    fun searchUniversities(@RequestBody searchParameters: JsonNode): Collection<University> {
+        val processedSearchParameters =
+            searchParameters.fields().asSequence().map { it.key to objectMapper.writeValueAsString(it.value) }.toMap()
+        return service.search(processedSearchParameters)
     }
 }
