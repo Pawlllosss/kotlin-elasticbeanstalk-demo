be1cbe489acce252744e2ec45ced36c159b3492f
Paweł Oczadly <oczadlypawe@gmail.com>
2023-09-30 20:27:11 +0200

    Finals subject filter (#2)
    * added FinalsSubjectCourseDegreeFilter
    
    * some after merge change

--- src/main/kotlin/com/wektorzabrze/coogle/university/CourseDegree.kt
+++ src/main/kotlin/com/wektorzabrze/coogle/university/CourseDegree.kt
@@ -3,5 +3,6 @@ package com.wektorzabrze.coogle.university
 data class CourseDegree(
     val name: String,
     val description: String = "",
-    val interests: Collection<String> = emptyList()
+    val finalsSubjects: List<String> = emptyList(),
+    val interests: Collection<String> = emptyList(),
 )
---
+++ src/test/kotlin/com/wektorzabrze/coogle/university/filters/coursedegree/FinalsSubjectCourseDegreeFilterTest.kt
@@ -0,0 +1,71 @@
+package com.wektorzabrze.coogle.university.filters.coursedegree
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.wektorzabrze.coogle.university.CourseDegree
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.equals.shouldBeEqual
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FinalsSubjectCourseDegreeFilterTest {
+
+    private val sut = FinalsSubjectCourseDegreeFilter(ObjectMapper())
+
+    @Test
+    fun `should return correct discriminator`() = assertEquals("finalsSubject", sut.discriminator)
+
+    @Test
+    fun `should filter based on multiple value filter`() {
+        //given
+        val expectedCourse1 = CourseDegree("Informatyka", finalsSubjects = listOf("matematyka", "informatyka"))
+        val expectedCourse2 = CourseDegree("Geoinformatyka", finalsSubjects = listOf("geografia", "informatyka"))
+        val courseDegrees = listOf(
+            expectedCourse1,
+            CourseDegree("Socjologia", finalsSubjects = listOf("polski", "historia")),
+            CourseDegree("Pielęgniarstwo", finalsSubjects = listOf("biologia")),
+            expectedCourse2,
+        )
+        val filterValue = """["Informatyka", "matEMatyka"]"""
+
+        //when
+        val result = sut.filter(courseDegrees, filterValue)
+
+        //then
+        result shouldContainAll listOf(expectedCourse1, expectedCourse2)
+    }
+
+    @Test
+    fun `should not filter out courses for empty filter list`() {
+        //given
+        val courseDegrees = listOf(
+            CourseDegree("Informatyka"),
+            CourseDegree("Socjologia")
+        )
+        val filterValue = """[]"""
+
+        //when
+        val result = sut.filter(courseDegrees, filterValue)
+
+        //then
+        result shouldContainAll listOf(
+            CourseDegree("Informatyka"),
+            CourseDegree("Socjologia")
+        )
+    }
+
+    @Test
+    fun `should filter courses with empty finals subjects`() {
+        //given
+        val courseDegrees = listOf(
+            CourseDegree("Informatyka"),
+            CourseDegree("Socjologia")
+        )
+        val filterValue = """["some test value"]"""
+
+        //when
+        val result = sut.filter(courseDegrees, filterValue)
+
+        //then
+        result.isEmpty() shouldBeEqual true
+    }
+}
